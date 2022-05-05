9f833e4f4d23f0ab820df912a3c454aad7d14035
Philip Welz <philip.welz@googlemail.com>
2022-05-05 15:00:23 +0200

    feat: update TF and provider

--- provider.tf
+++ provider.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.3.0"
+      version = "=3.4.0"
     }
     azapi = {
-      source = "Azure/azapi"
+      source  = "Azure/azapi"
       version = "=0.1.1"
     }
   }
 
-  required_version = "=1.1.7"
+  required_version = "=1.1.9"
 }
 
 provider "azurerm" {
